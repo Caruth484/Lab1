ed5456cdfbf7038bcf3d5d4944dfa38e56bcafd7
Carter Dewey <CH484-mac1@usr127-47.perunanet.smu.edu>
2015-02-03 19:54:30 -0600

    Tableview with collection view start.
    I have created a table view a segue to my collection view. If you look
    in the TableViewController.m file i marked a spot for y’all to enter
    your own segues to your views.

---
+++ Lab1/Lab1/CollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.h
+//  Lab1
+//
+//  Created by ch484-mac3 on 2/3/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+@end
---
+++ Lab1/Lab1/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  SMUExampleOne
+//
+//  Created by Eric Larson on 1/21/15.
+//  Copyright (c) 2015 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (strong,nonatomic) NSArray* tableCellNames;
+@property (strong,nonatomic) NSArray* tableCellInfo;
+
+@end
